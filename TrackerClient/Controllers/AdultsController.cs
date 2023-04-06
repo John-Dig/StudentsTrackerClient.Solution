b072d35cc657d20716ba23243841e468d6b47894
John Diggins <rohnfiggins@gmail.com>
2023-04-05 22:13:00 -0700

    Added Adults controller, details, index.  Currently it is set to mirror students controller, only rendering will make the difference.
    Co-authored-by: Mike Donovan <mike@mikedonovan.dev>
    Co-authored-by: Chris <johnedisc@users.noreply.github.com>

---
+++ TrackerClient/Controllers/AdultsController.cs
@@ -0,0 +1,49 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TrackerClient.Models;
+
+namespace TrackerClient.Controllers;
+
+public class AdultsController : Controller
+{
+
+    
+    public IActionResult Index()
+    {
+        List<Student> students = Student.GetStudents();
+        return View(students);
+    }
+    public IActionResult Details(int id)
+    {
+        Student thisStudent = Student.GetDetails(id);
+        return View(thisStudent);
+    }
+
+    [HttpPost]
+    public IActionResult Create(Student student)
+    {
+
+        Student.Post(student);
+        return RedirectToAction("Index","Home");
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Student student)
+    {
+        Student.Put(student);
+        return RedirectToAction("Details","Students", new { id = student.StudentId });
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int id)
+    {
+        Student.Delete(id);
+        return RedirectToAction("Index","Home");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
